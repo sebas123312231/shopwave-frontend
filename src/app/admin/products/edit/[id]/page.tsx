ff36f3645f9554af37d53e2f4c18c6e2960f047e
sebas123312231 <arceantezanasebastianalejandro@gmail.com>
2026-05-28 22:29:23 -0400

    fixes

--- src/app/admin/products/edit/[id]/page.tsx
+++ src/app/admin/products/edit/[id]/page.tsx
@@ -59,7 +59,7 @@ export default function AdminEditProductPage() {
 
   return (
     <AdminGuard>
-    <div className="max-w-3xl mx-auto">
+    <div className="max-w-3xl mx-auto md:px-8 py-6 md:py-8">
       <div className="mb-8">
         <Button variant="ghost" onClick={() => router.push('/admin/products')} className="mb-4">
           <ArrowLeft size={18} />
@@ -105,3 +105,4 @@ export default function AdminEditProductPage() {
     </AdminGuard>
   );
 }
+//a
